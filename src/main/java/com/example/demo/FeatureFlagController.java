1f7317f4012dd3750d95600bcf1d4a79bedc411c
Brian Flanagan <bpflanag@mtu.edu>
2020-11-13 18:47:46 -0500

    BF NK | Allow cross origin headers on isEnabled endpoint

--- src/main/java/com/example/demo/FeatureFlagController.java
+++ src/main/java/com/example/demo/FeatureFlagController.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/features")
@@ -12,6 +9,7 @@ public class FeatureFlagController {
 
     private boolean featureFlag;
 
+    @CrossOrigin(allowedHeaders = "*")
     @GetMapping(value = "/isEnabled")
     public boolean isEnabled() {
         return featureFlag;
